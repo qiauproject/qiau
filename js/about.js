d4a48791290ce5db0281a3ec6fd16da35ed8cb4e
qiauproject <qiauproject.00@gmail.com>
2016-07-15 21:44:14 +0430

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,3 +1,21 @@
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope,$http) {
+    url = "http://api.openweathermap.org/data/2.5/forecast?q=Qazvin,ir&units=metric&appid=c90fc1475e237f7aafd567d3a6deff71";
+    $http.get(url)
+        .success(function (data) {
+            $scope.data = data;
+            $scope.class0 = "owf-" + data.list[0].weather[0].id ;
+            $scope.class1 = "owf-" + data.list[1].weather[0].id ;
+            $scope.class2 = "owf-" + data.list[2].weather[0].id ;
+            $scope.class3 = "owf-" + data.list[3].weather[0].id ;
+
+        })
+        .error(function () {
+        });
+});
+
+
 var myCenter=new google.maps.LatLng(36.3228975,50.031462);
 var marker;
 
@@ -23,21 +41,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope,$http) {
-    url = "http://api.openweathermap.org/data/2.5/forecast?q=Qazvin,ir&units=metric&appid=c90fc1475e237f7aafd567d3a6deff71";
-    $http.get(url)
-        .success(function (data) {
-            $scope.data = data;
-            $scope.class0 = "owf-" + data.list[0].weather[0].id ;
-            $scope.class1 = "owf-" + data.list[1].weather[0].id ;
-            $scope.class2 = "owf-" + data.list[2].weather[0].id ;
-            $scope.class3 = "owf-" + data.list[3].weather[0].id ;
 
-        })
-        .error(function () {
-        });
-});
 
 
 
